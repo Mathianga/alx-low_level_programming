cb259feeea9fb890c9b6e66cd67abe33fb4431e2
mathianga <gabrielmathiang1@gmail.com>
2023-05-18 14:40:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This programe is to prints a line of text with a new line
+ *
+ * Return: Always (0) on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
