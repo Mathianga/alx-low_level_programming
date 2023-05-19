2ec510053e5f842a8bd59fbb134df5a8551a0a22
mathianga <gabrielmathiang1@gmail.com>
2023-05-19 15:28:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m <= 10; m++)
+		putchar(m);
+	putchar('\n');
+	return (0);
+}
