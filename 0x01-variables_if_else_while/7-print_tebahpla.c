c38fb3c4fa2a87d4666c1e40a090f583faef935d
mathianga <gabrielmathiang1@gmail.com>
2023-05-19 15:46:37 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char n;
+
+	n = 'z';
+	while (n >= 'a')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
