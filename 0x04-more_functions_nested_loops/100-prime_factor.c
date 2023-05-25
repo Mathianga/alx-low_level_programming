5b95d70da354eb4d3b9cbf35e3dff2afaf8073d9
mathianga <gabrielmathiang1@gmail.com>
2023-05-25 15:55:09 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,32 +9,32 @@
 
 int main()
 {
-	long int q;
+	long int n;
+	long int max;
 	long int i;
-	long int maxi;
 
-	q = 612852475143;
-	maxi = -1;
+	n = 612852475143;
+	max = -1;
 
-	while (q % 2 == 0)
+	while (n % 2 == 0)
 	{
-		maxi = 2;
-		q /= 2;
+		max = 2;
+		n /= 2;
 	}
 
-	for (i = 3; i <= sqrt(q); i = i + 2)
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-		while (q % i == 0)
+		while (n % i == 0)
 		{
-			maxi = i;
-			q = q / i;
+			max = i;
+			n = n / i;
 		}
 	}
 
-	if (q > 2)
-		maxi = q;
+	if (n > 2)
+		max = n;
 
-	printf("%ld\n", maxi);
+	printf("%ld\n", max);
 
 	return (0);
 }
