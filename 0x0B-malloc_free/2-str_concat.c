b27672318c1276d2c1d542cb1a1e903bc5a2bf44
mathianga <gabrielmathiang1@gmail.com>
2023-06-13 07:07:10 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,14 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *conct;
-	int len1 = 0, len2 = 0;
+	int len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
+	len1 = len2 = 0;
 	while (s1[len1] != '\0')
 		len1++;
 	while (s2[len2] != '\0')
@@ -31,14 +32,16 @@ char *str_concat(char *s1, char *s2)
 	while (s1[len1] != '\0')
 	{
 		conct[len1] = s1[len1];
-		len2++;
+		len1++;
 	}
 
 	while (s2[len2] != '\0')
 	{
-		conct[len2] = s2[len2];
-		len1++, len2++;
+		conct[len1] = s2[len2];
+		len1++;
+		len2++;
 	}
-	conct[len1] = '\0';
+	conct[len2] = '\0';
+
 	return (conct);
 }
